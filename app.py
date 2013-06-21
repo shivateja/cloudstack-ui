f765258c9b4122121eaa4a81178b75278ea98bdf
Shiva Teja Reddy <shivatejau@gmail.com>
2013-06-22 01:10:59 +0530

    Serve template directly for now, as Jinja is interfering with angular expressions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, json, abort
+from flask import Flask, url_for, render_template, request, json, abort, send_from_directory
 from api import apicall
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ def onlysubject(subject):
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return send_from_directory("templates", "index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
